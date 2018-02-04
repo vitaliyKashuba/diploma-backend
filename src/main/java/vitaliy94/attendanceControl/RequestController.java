b31594e359e0e22d187540c70960d8df2229a571
vitaliy <k.vitaliy1994@gmail.com>
2018-02-04 23:36:39 +0200

    almost finish with getStats()

--- src/main/java/vitaliy94/attendanceControl/RequestController.java
+++ src/main/java/vitaliy94/attendanceControl/RequestController.java
@@ -8,10 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vitaliy94.attendanceControl.model.Lecturers;
 import vitaliy94.attendanceControl.model.Schedule;
+import vitaliy94.attendanceControl.model.VisitingInfo;
 import vitaliy94.attendanceControl.util.AppUtil;
 import vitaliy94.attendanceControl.util.HibernateUtil;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -57,9 +59,24 @@ public class RequestController
         Lecturers lect = (Lecturers) query.list().get(0);
         System.out.println(lect.getName());
         Collection<Schedule> schedule = lect.getSchedulesById();
+        HashMap<String, Integer> stats = new HashMap<String, Integer>();
         for(Schedule s : schedule)
         {
-            System.out.println(s.getId() + " " + s.getTime() + " " + s.getClass());
+            String subjName = s.getSubjectsBySubjectId().getName();
+            Query q = session.createQuery("from VisitingInfo where scheduleId = " + s.getId());
+            int studCount = q.list().size();
+            if(stats.containsKey(subjName))
+            {
+                stats.replace(subjName, stats.get(subjName)+studCount);
+            }
+            else
+            {
+                stats.put(subjName, studCount);
+            }
+        }
+        for(String k : stats.keySet())
+        {
+            System.out.println(k + " " + stats.get(k));
         }
         return String.valueOf(id);
     }
